d46a43a2f1dccc2b94e526c8bf6de23ebe6fd378
Sam Yuen <117583090+sfyuen@users.noreply.github.com>
2023-11-20 02:40:59 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,69 +1,117 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
 function App() {
   return (
-    <Container className='grid'>
+    <Container>
       <Row>
-        <div className="display">
-          0
-        </div>
-      </Row>
-      <Row className='justify-content-center'>
-        <Col sm={3}>
-        <div className="square"><Button variant="Primary">7</Button>{' '}</div>
-        </Col>
-        <Col sm={3}>
-        <div className="square"><Button variant="Primary">8</Button>{' '}</div>
-        </Col>
-        <Col sm={3}>
-        <div className="square"><Button variant="Primary">9</Button>{' '}</div>
-        </Col>
-        <Col sm={3}>
-        <div className="square"><Button variant="Secondary">×</Button>{' '}</div>
-        </Col>
+        <div className="display">0</div>
       </Row>
       <Row>
         <Col sm={3}>
-        <div className="square"><Button variant="Primary">4</Button>{' '}</div>
+          <div className="square">
+            <Button variant="Info">C</Button>{" "}
+          </div>
         </Col>
         <Col sm={3}>
-        <div className="square"><Button variant="Primary">5</Button>{' '}</div>
+          <div className="square">
+            <Button variant="Secondary">÷</Button>{" "}
+          </div>
         </Col>
         <Col sm={3}>
-        <div className="square"><Button variant="Primary">6</Button>{' '}</div>
+          <div className="square">
+            <Button variant="Secondary">×</Button>{" "}
+          </div>
         </Col>
         <Col sm={3}>
-        <div className="square"><Button variant="Secondary">−</Button>{' '}</div>
+          <div className="square">
+            <Button variant="Secondary">−</Button>{" "}
+          </div>
         </Col>
       </Row>
       <Row>
-        <Col sm={3}>
-        <div className="square"><Button variant="Primary">1</Button>{' '}</div>
+        <Col sm={9}>
+          <Row>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">7</Button>{" "}
+              </div>
+            </Col>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">8</Button>{" "}
+              </div>
+            </Col>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">9</Button>{" "}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">4</Button>{" "}
+              </div>
+            </Col>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">5</Button>{" "}
+              </div>
+            </Col>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">6</Button>{" "}
+              </div>
+            </Col>
+          </Row>
         </Col>
         <Col sm={3}>
-        <div className="square"><Button variant="Primary">2</Button>{' '}</div>
-        </Col>
-        <Col sm={3}>
-        <div className="square"><Button variant="Primary">3</Button>{' '}</div>
-        </Col>
-        <Col sm={3}>
-        <div className="square"><Button variant="Secondary">+</Button>{' '}</div>
+          <div className="hrect">
+            <Button variant="Secondary">+</Button>{" "}
+          </div>
         </Col>
       </Row>
-      <Row className='justify-content-end'>
-        <Col>
-        <div className="rect"><Button variant="Primary">0</Button>{' '}</div>
+      <Row>
+        <Col sm={9}>
+          <Row>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">1</Button>{" "}
+              </div>
+            </Col>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">2</Button>{" "}
+              </div>
+            </Col>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">3</Button>{" "}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={8}>
+              <div className="vrect">
+                <Button variant="Primary">0</Button>{" "}
+              </div>
+            </Col>
+            <Col sm={4}>
+              <div className="square">
+                <Button variant="Primary">.</Button>{" "}
+              </div>
+            </Col>
+          </Row>
         </Col>
         <Col sm={3}>
-        <div className="square"><Button variant="Secondary">÷</Button>{' '}</div>
-        </Col>
-        <Col sm={3}>
-        <div className="square"><Button variant="Secondary">=</Button>{' '}</div>
+          <div className="hrect">
+            <Button variant="Secondary">=</Button>{" "}
+          </div>
         </Col>
       </Row>
     </Container>
